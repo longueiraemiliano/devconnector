6728893cb1fa8f05d709f4179f716521a4276f7c
Longueira, Emiliano <longueiraemiliano@gmail.com>
2018-06-11 14:24:06 -0300

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,4 +44,31 @@ router.post('/register', (req, res) => {
         });
 });
 
+// @route   GET api/users/login
+// @desc    Login user
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // FInd user by email
+    User.findOne({email})
+        .then(user => {
+            // CHeck for user
+            if(!user) {
+                return res.status(404).json({email: 'User not found'});
+            }
+
+            // Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({msg: 'Success'});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'});
+                    }
+                });
+        })
+});
+
 module.exports = router;
