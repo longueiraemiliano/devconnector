b98c164e7f373661e1649d548746e09a7c67aebb
longueiraemiliano <longueiraemiliano@gmail.com>
2018-06-26 20:44:44 -0300

    register user

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; {new Date().getFullYear()} Dev Connector
-    </footer>
-  )
+class Footer extends Component {
+  render () {
+    return (
+      <footer className="bg-dark text-white mt-5 p-4 text-center">
+          Copyright &copy; {new Date().getFullYear()} Dev Connector
+      </footer>
+    )
+  }
 }
+
+export default Footer
