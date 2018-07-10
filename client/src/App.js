1912258dbf82a63514c5c1f2fb60bf18ab459db6
Longueira, Emiliano <longueiraemiliano@gmail.com>
2018-07-10 16:57:09 -0300

    auth action and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import store  from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -10,7 +10,6 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
