8b3ad68938a7c07f023946d604a212d5128b24da
longueiraemiliano <longueiraemiliano@gmail.com>
2018-11-22 12:15:33 -0300

    experience and education features

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -43,5 +43,52 @@ export const setProfileLoading = () => {
 export const clearCurrentProfile = () => {
     return {
         type: CLEAR_CURRENT_PROFILE
+    }
+}
+
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post("/api/profile/experience", expData)
+        .then(res => {
+            history.push("/dashboard")
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post("/api/profile/education", eduData)
+        .then(res => {
+            history.push("/dashboard")
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+export const deleteAccount = () => dispatch => {
+    if (window.confirm("Are you sure? This can NOT be undone")) {
+        axios
+            .delete("/api/profile")
+            .then(res => {
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            })
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                })
+            })
     }
 }
